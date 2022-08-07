df8b8de6c4613a55d5b371bd51be0d1f3f54dc69
Iain J. Reid <iainreid382@gmail.com>
2022-08-07 19:27:49 +0000

    chore: Split out Promise implementation to separate lib

--- lib/compose.d.ts
+++ lib/compose.d.ts
@@ -1,7 +1,7 @@
-import { Promise } from "./promise";
+import type { Promise } from "@emphori/promise";
 
 export declare type ComposableFunction<C1, I1 extends any[], R1, E1> =
-  ((this: C1, ...args: I1) => Promise<R1, E1>);
+  (this: C1, ...args: I1) => Promise<R1, E1>;
 
 /**
  * If you see this error message when building your code, it's likely that
@@ -10,20 +10,20 @@ export declare type ComposableFunction<C1, I1 extends any[], R1, E1> =
 export type UnreachableFunctionWarning = "Your function will never be run";
 
 /**
- *
+ * @todo Document the "UnaryComposableFunction" type
  */
 export declare type UnaryComposableFunction<C1, R1, R2, E1> =
   [R1] extends [never] ? UnreachableFunctionWarning : ComposableFunction<C1, [R1], R2, E1>;
 
 /**
- *
+ * @todo Document the "Composition" interface
  */
 export interface Composition<C1, I1 extends any[], R1, E1> extends ComposableFunction<C1, I1, R1, E1> {
   then<R2, __, C2 extends C1 = C1>(fn: UnaryComposableFunction<C2, R1, R2, never>):
     Composition<C2, I1, R2, E1>;
 
   then<R2, E2, C2 extends C1 = C1>(fn: UnaryComposableFunction<C2, R1, R2, E2>):
-    Composition<C2, I1, R2, E1 | E2>;
+    Composition<C2, I1, R2, E2 | E1>;
 
   catch<__, C2 extends C1 = C1>(fn: UnaryComposableFunction<C2, E1, R1, never>):
     Composition<C2, I1, R1, never>;
@@ -33,6 +33,7 @@ export interface Composition<C1, I1 extends any[], R1, E1> extends ComposableFun
 }
 
 /**
+ * @todo Document the "compose" function
  *
  * @param fn
  */
